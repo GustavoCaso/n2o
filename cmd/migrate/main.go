9245ef2b15767b3b28f9648cbf1e6be6b6e60482
Gustavo Caso <gustavocaso@gmail.com>
2024-02-09 22:24:50 +0100

    created cache internal package

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -13,55 +13,14 @@ import (
 	"path"
 	"path/filepath"
 	"strings"
-	"sync"
 
+	"github.com/GustavoCaso/n2o/internal/cache"
 	"github.com/GustavoCaso/n2o/internal/queue"
 	"github.com/dstotijn/go-notion"
 	"github.com/itchyny/timefmt-go"
 )
 
-type cache struct {
-	storage map[string]string
-	working map[string]bool
-	mu      sync.RWMutex
-}
-
-func (c *cache) Get(value string) (string, bool) {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
-	val, ok := c.storage[value]
-	return val, ok
-}
-
-func (c *cache) Set(key, value string) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	c.storage[key] = value
-	c.working[key] = false
-}
-
-func (c *cache) Mark(key string) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	c.working[key] = true
-}
-
-func (c *cache) IsWorking(key string) bool {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	return c.working[key]
-}
-
-func newCache() *cache {
-	storage := map[string]string{}
-	working := map[string]bool{}
-	return &cache{
-		storage: storage,
-		working: working,
-	}
-}
-
-var mentionCache = newCache()
+var mentionCache = cache.NewCache()
 
 type pathAttributes map[string]string
 
